f193b79cf5344766ccf58342268e6b2234d02c11
Stephanie Mayers <g00286470@stu.raritanval.edu>
2022-03-23 15:34:43 -0400

    Web Server with HTML Files

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const http = require("http");
+const fs = require ("fs")
 
 const PORT = process.env.PORT || 3000;
 
  const server = http.createServer((req, res) => {
+     console.log(req.url. req.method)
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html");
-    response.end('<h1>Hello World<h1>');
-})
+    fs.readFile('./index.html', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.end();
+        } else {
+        res.end(data)
+        }
+    })
+});
+
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
